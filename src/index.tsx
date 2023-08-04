c681c570770a4ac4cacbd6eb434305328db91a97
Pavel <khuzdblacksmith@gmail.com>
2023-08-04 03:25:36 +0300

    back

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -15,7 +15,6 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <Suspense fallback ={<div>Loading...</div>} />
     <I18nextProvider i18n={i18n}>
       <Provider store={store}>
         <App />
